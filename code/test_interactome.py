88f2d367a3da5cbaeb2b450bca4c0036bcd4ff6d
nhoffmann <a>
2022-11-24 21:40:59 +0100

    Added tests

--- code/test_interactome.py
+++ code/test_interactome.py
@@ -15,6 +15,16 @@ def test_attributes():
     assert test_ppi.list == [('A', 'B'), ('A','C'), ('B', 'C'), ('B', 'D'), ('D', 'E'), ('D', 'F')]
     assert test_ppi.protein == ['A', 'B', 'C', 'D', 'E', 'F']
 
+def test_attributes_toy2():
+    test_file = Path(__file__).parent / '../example_files/toy2.txt'
+    test_ppi = interactome.Interactome(test_file)
+    assert test_ppi.dict == {'A': ['G', 'C'], 'B': ['H'], 'C': ['A', 'G'], \
+        'D': ['G', 'E', 'F'], 'E': ['D'], 'F': ['D'], 'G': ['A', 'C', 'D'], \
+        'H': ['B'], 'I': ['J'], 'J': ['I', 'K'], 'K': ['J']}
+    assert test_ppi.list == [('A', 'C'), ('A', 'G'), ('B', 'H'), ('C', 'G'), \
+        ('D', 'E'), ('D', 'F'), ('D', 'G'), ('I', 'J'), ('J', 'K')]
+    assert test_ppi.protein == ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K']
+
 def test_methods():
     assert test_ppi.count_edges() == 6
     assert test_ppi.count_vertices() == 6
@@ -27,6 +37,21 @@ def test_methods():
     assert test_ppi.clustering("B") == 0.3333333333333333
     assert test_ppi.clustering("F") == 0
     assert test_ppi.count_cc() == (1, [6])
+
+def test_methods_toy2():
+    test_file = Path(__file__).parent / '../example_files/toy2.txt'
+    test_ppi = interactome.Interactome(test_file)
+    assert test_ppi.count_edges() == 9
+    assert test_ppi.count_vertices() == 11
+    assert test_ppi.get_degree("B") == 1
+    assert test_ppi.get_degree("E") == 1
+    assert test_ppi.get_max_degree() == (["D","G"], 3)
+    assert test_ppi.get_ave_degree() == 1.6
+    assert test_ppi.count_degree(2) == 3
+    assert test_ppi.density() == 0.16363636363636364
+    assert test_ppi.clustering("B") == 0
+    assert test_ppi.clustering("F") == 0
+    assert test_ppi.count_cc() == (3, [6,2,3])
 
 def test_cc():
     test_file = Path(__file__).parent / '../example_files/toy2.txt'
